c408ac4f74355e7290672c263b88f1eb58454f1e
Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>
2024-02-15 15:01:01 +0300

    WIP: ready for testing
    Signed-off-by: Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>

--- src/shape.rs
+++ src/shape.rs
@@ -6,6 +6,7 @@
 use alloc::vec::Vec;
 use core::cmp::{max, min};
 use core::fmt;
+use core::iter::Rev;
 use core::mem;
 use core::ops::Range;
 use unicode_script::{Script, UnicodeScript};
@@ -1038,7 +1039,15 @@ impl ShapeLine {
             };
 
             let skip_before = custom_split.skip_before.unwrap_or_default();
-            let skip_after = custom_split.skip_after.unwrap_or_default();
+            let skip_after = custom_split.skip_after.unwrap_or(usize::MAX);
+
+            let mut line_endings = custom_split.new_lines_at
+                .iter()
+                .filter(|&&line_ending| line_ending > skip_before)
+                .copied();
+
+            let mut curr_line_ending = line_endings.next().unwrap_or(skip_after);
+
             let mut skipped_spans = 0;
 
             for (mut span_index, span) in self.spans.iter().enumerate() {
@@ -1052,24 +1061,52 @@ impl ShapeLine {
                 span_index -= skipped_spans;
                 let mut skipped_words = 0;
                 let mut skipped_glyphs = 0;
-                let mut last_included_glyph_i = 0;
 
                 let mut word_range_width = 0.;
                 let mut number_of_blanks: u32 = 0;
 
-                // Create the word ranges that fits in a visual line
-                if self.rtl != span.level.is_rtl() {
-                    todo!()
-                } else {
-                    // congruent direction
-                    //let mut fitting_start = (0, 0);
-                    'WORDS: for (i, word) in span.words.iter().enumerate() {
-                        let min_start = word_min_start(word);
-                        let max_end = word_max_end(word);
+                let incongruent_span = self.rtl != span.level.is_rtl();
 
-                        if min_start < skip_before && max_end > skip_after {
-                            skipped_words += 1;
-                        } else if min_start >= skip_before && max_end <= skip_after {
+                enum ForwardReverseIter<T, I: Iterator<Item=T> + DoubleEndedIterator> {
+                    Forward(I),
+                    Reverse(Rev<I>),
+                }
+
+                impl<T, I: Iterator<Item=T> + DoubleEndedIterator> Iterator for ForwardReverseIter<T, I> {
+                    type Item = T;
+
+                    fn next(&mut self) -> Option<Self::Item> {
+                        match self {
+                            Self::Forward(iter) => iter.next(),
+                            Self::Reverse(iter) => iter.next(),
+                        }
+                    }
+                }
+
+                // Create the word ranges that fits in a visual line
+                let (mut start, words_iter) = if incongruent_span {
+                    (
+                        (span.words.len(), 0),
+                        ForwardReverseIter::Reverse(span.words.iter().enumerate().rev()),
+                    )
+                } else {
+                    (
+                        (0, 0),
+                        ForwardReverseIter::Forward(span.words.iter().enumerate()),
+                    )
+                };
+
+                'WORDS: for (i, word) in words_iter {
+                    let min_start = word_min_start(word);
+                    let max_end = word_max_end(word);
+
+                    if min_start < skip_before && max_end > skip_after {
+                        skipped_words += 1;
+                        continue 'WORDS;
+                    }
+
+                    if min_start >= skip_before && max_end <= skip_after {
+                        if max_end <= curr_line_ending {
                             // word fully outside skipped ranges
                             let word_width = font_size * word.x_advance;
 
@@ -1078,48 +1115,85 @@ impl ShapeLine {
                             }
 
                             word_range_width += word_width;
-                            continue 'WORDS;
-                        } else {
-                            // word partially outside skipped ranges
-                            'GLYPHS: for (glyph_i, glyph) in word.glyphs.iter().enumerate() {
-                                // We deliberately only check for skip_before here. If a glyph
-                                // starts before skip_after, we consider it in layout range.
-                                if glyph.start >= skip_before  {
-                                    let glyph_width = font_size * glyph.x_advance;
-                                    word_range_width += glyph_width;
-                                    last_included_glyph_i = glyph_i;
-                                    continue 'GLYPHS;
-                                } else {
-                                    skipped_glyphs += 1;
-                                    /*
-                                    add_to_visual_line(
-                                        &mut current_visual_line,
-                                        span_index,
-                                        fitting_start,
-                                        (i - skipped_words, glyph_i - skipped_glyphs),
-                                        word_range_width,
-                                        number_of_blanks,
-                                    );
-                                    visual_lines.push(current_visual_line);
-                                    current_visual_line = VisualLine::default();
 
-                                    number_of_blanks = 0;
-                                    word_range_width = glyph_width;
-                                    fitting_start = (i, glyph_i);
-                                    */
-                                }
+                            if max_end == curr_line_ending {
+                                add_to_visual_line(
+                                    &mut current_visual_line,
+                                    span_index,
+                                    start,
+                                    (i - skipped_words, 0),
+                                    word_range_width,
+                                    number_of_blanks,
+                                );
+                                visual_lines.push(current_visual_line);
+                                current_visual_line = VisualLine::default();
+                                curr_line_ending = line_endings.next().unwrap_or(skip_after);
+
+                                number_of_blanks = 0;
+                                word_range_width = 0.0;
+                                start = (i - skipped_words, 0);
                             }
+                            continue 'WORDS;
                         }
-                        add_to_visual_line(
-                            &mut current_visual_line,
-                            span_index,
-                            (0, 0),
-                            (span.words.len() - skipped_words, last_included_glyph_i - skipped_glyphs),
-                            word_range_width,
-                            number_of_blanks,
-                        );
+                    }
+
+                    // word partially outside line range
+                    let glyphs_iter = if incongruent_span {
+                        ForwardReverseIter::Reverse(word.glyphs.iter().enumerate().rev())
+                    } else {
+                        ForwardReverseIter::Forward(word.glyphs.iter().enumerate())
+                    };
+
+                    'GLYPHS: for (glyph_i, glyph) in glyphs_iter {
+                        // We deliberately only check for skip_before here. If a glyph
+                        // starts before skip_after, we consider it in layout range.
+                        if glyph.start >= skip_before {
+                            let glyph_width = font_size * glyph.x_advance;
+
+                            if glyph.end <= curr_line_ending {
+                                word_range_width += glyph_width;
+                                continue 'GLYPHS;
+                            } else {
+                                add_to_visual_line(
+                                    &mut current_visual_line,
+                                    span_index,
+                                    (i - skipped_words, glyph_i + 1 - skipped_glyphs),
+                                    start,
+                                    word_range_width,
+                                    number_of_blanks,
+                                );
+                                visual_lines.push(current_visual_line);
+                                current_visual_line = VisualLine::default();
+                                curr_line_ending = line_endings.next().unwrap_or(usize::MAX);
+
+                                number_of_blanks = 0;
+                                word_range_width = glyph_width;
+                                start = (i - skipped_words, glyph_i + 1 - skipped_glyphs);
+                            }
+                        } else {
+                            skipped_glyphs += 1;
+                        }
                     }
                 }
+                let (full_span_start, full_span_end) = if incongruent_span {
+                    (
+                        (0, 0),
+                        start,
+                    )
+                } else {
+                    (
+                        start,
+                        (span.words.len() - skipped_words, 0),
+                    )
+                };
+                add_to_visual_line(
+                    &mut current_visual_line,
+                    span_index,
+                    full_span_start,
+                    full_span_end,
+                    word_range_width,
+                    number_of_blanks,
+                );
             }
         } else if wrap == Wrap::None {
             for (span_index, span) in self.spans.iter().enumerate() {
